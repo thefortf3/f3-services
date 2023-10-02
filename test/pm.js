cfbbbbd792e3906cc8e400519cd7de6d003ec943
Andrew <andrew@thelucks.org>
2023-10-02 14:31:51 -0400

    Updating with new table structure

--- test/pm.js
+++ test/pm.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 const pm = require("../lib/paxminer");
 
 
-// pm.getBBs((results) => {
-//     results.forEach(result => {
-//         console.log("AO " + result.ao + " last posted a BB " + result.age + " days ago");
-//     });
-//     console.log(results);
+pm.getBBs((results) => {
+    results.forEach(result => {
+        console.log("AO " + result.ao + " last posted a BB " + result.age + " days ago");
+    });
+    console.log(results);
 
-//     process.exit();
-// });
-
-pm.addVQ('2023-02-11', 'Console', 'The Ranch', (err) => {
-    console.error("ERROR")
-    process.exit()
+    process.exit();
 });
+
+// pm.addVQ('2023-02-11', 'Console', 'The Ranch', (err) => {
+//     console.error("ERROR")
+//     process.exit()
+// });
     
